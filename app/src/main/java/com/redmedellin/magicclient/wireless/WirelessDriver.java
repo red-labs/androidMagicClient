3ca39cef6e3d3877aaa83f08a7679c725f9b75d6
Alex <a.pessina@yandex.ru>
2019-07-10 18:34:39 +0200

    Create WirelessDriver interface  for wifi management

---
+++ app/src/main/java/com/redmedellin/magicclient/wireless/WirelessDriver.java
@@ -0,0 +1,23 @@
+package com.redmedellin.magicclient.wireless;
+
+public interface WirelessDriver {
+
+    //Check if 802.1x credentials are already installed
+    public  boolean has8021xCreds(String ssid, String username, String password);
+
+    //Install/add 802.1x credentials to Android
+    public void install8021xCreds(String ssid, String username, String password);
+
+    //Connect to the network specified by ssid.
+    public void connect(String ssid);
+
+    //Get current network's SSID
+    public String getCurrentSsid();
+
+    //Scan for available wifi networks
+    public void scanNetworks();
+
+    //Get the details of the current network interface or adapter
+    public String getAdapter();
+
+}
