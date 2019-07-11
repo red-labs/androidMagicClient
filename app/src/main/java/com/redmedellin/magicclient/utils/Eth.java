346344c35cc1198c3ee781b94088a76d877f9cd9
Alex <a.pessina@yandex.ru>
2019-07-11 21:46:15 +0200

    Initial implementation of account creation and message signature methods in Ethereum util class Eth.java

--- app/src/main/java/com/redmedellin/magicclient/utils/Eth.java
+++ app/src/main/java/com/redmedellin/magicclient/utils/Eth.java
@@ -2,10 +2,14 @@ package com.redmedellin.magicclient.utils;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.web3j.crypto.Credentials;
 import org.web3j.crypto.ECKeyPair;
+import org.web3j.crypto.Hash;
 import org.web3j.crypto.Keys;
+import org.web3j.crypto.Sign;
 import org.web3j.crypto.Wallet;
 import org.web3j.crypto.WalletFile;
+import org.web3j.utils.Numeric;
 
 import java.math.BigInteger;
 import java.security.InvalidAlgorithmParameterException;
@@ -30,8 +34,13 @@ public class Eth {
         return accountInfo;
     }
 
-    public static String sign(String s, Object privkey) {
-        //TODO code to sign a message
-        return "";
+    public static Sign.SignatureData sign(String message, String privkey) {
+
+        Credentials credentials = Credentials.create(privkey); //Generate a temp credentials object so signMessage function accepts it
+        String hash = Hash.sha3(Numeric.toHexStringNoPrefix(message.getBytes())); 
+        String completeMessage = "\\x19Ethereum Signed Message:\n" + hash.length() + hash; //Taken from https://github.com/web3j/web3j/issues/208
+        byte[] data = completeMessage.getBytes();
+        Sign.SignatureData signed = Sign.signMessage(data, credentials.getEcKeyPair());
+        return signed;
     }
 }
