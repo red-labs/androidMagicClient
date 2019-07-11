c4ae306a2241eddd96fe97069ea5ebd52a2524ac
Alex <a.pessina@yandex.ru>
2019-07-11 17:10:54 +0200

    Increase min API version to v18 and initial implementation of install8021xCreds method

--- app/src/main/java/com/redmedellin/magicclient/wireless/Wireless.java
+++ app/src/main/java/com/redmedellin/magicclient/wireless/Wireless.java
@@ -1,6 +1,17 @@
 package com.redmedellin.magicclient.wireless;
 
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiEnterpriseConfig;
+
 public class Wireless implements WirelessDriver{
+
+    WifiConfiguration config;
+    WifiEnterpriseConfig enterpriseConfig;
+
+    public Wireless(){
+        config = new WifiConfiguration();
+        enterpriseConfig = new WifiEnterpriseConfig();
+    }
 
     @Override
     public boolean has8021xCreds(String ssid, String username, String password) {
@@ -10,11 +21,32 @@ public class Wireless implements WirelessDriver{
     @Override
     public void install8021xCreds(String ssid, String username, String password, String timestamp) {
 
+        //Add the SSID info to the basic wifi config object, and enable it
+        config.SSID = "\"" + ssid + "\"";
+        config.priority = 1;
+        config.status = WifiConfiguration.Status.ENABLED;
+
+        //Tell the config object we'll be using EAP and 802.1x
+        config.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_EAP);
+        config.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.IEEE8021X);
+
+        //Set up the enterprise config parameters
+        enterpriseConfig.setIdentity(username); //is the address parameter in linux_nmcli
+        String pw = password + "-" + timestamp; //as per original Python client format
+        enterpriseConfig.setPassword(pw);
+
+        enterpriseConfig.setEapMethod(WifiEnterpriseConfig.Eap.TTLS); //TTLS option from Python cli
+        enterpriseConfig.setPhase2Method(WifiEnterpriseConfig.Phase2.PAP);
+
+        //Add the enterprise configs to the basic WifiConfiguration object
+        config.enterpriseConfig = this.enterpriseConfig;
+
     }
 
     @Override
     public void connect(String ssid) {
 
+        config.SSID = ssid;
     }
 
     @Override
